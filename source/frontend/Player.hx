1c00a3b9e668f49bb8b5e5ee4f5947806374cf3a
Nicholas Demetro <itssng7@gmail.com>
2023-02-06 18:52:12 -0500

    br

--- source/frontend/Player.hx
+++ source/frontend/Player.hx
@@ -7,25 +7,15 @@ import flixel.FlxG;
 class Player extends FlxSprite
 {
 	var speed:Int = 150;
+
 	public function new(x:Float, y:Float)
 	{
 		super(x, y);
+
 		makeGraphic(16, 16, FlxColor.WHITE);
 		acceleration.y = 400;
 		maxVelocity.y = 200;
 		drag.x = speed * 4;
-	}
-
-	function movement(move:FlxMovement = STILL);
-	{
-		if (move == LEFT)
-		{
-			player.velocity.x = -speed
-		}
-		else if (move == RIGHT)
-		{
-			player.velocity.x = speed
-		}
 	}
 }
 
