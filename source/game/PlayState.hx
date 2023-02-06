45ffc34abbfbc839706481c0e86d12ae702f4fdd
Nicholas Demetro <itssng7@gmail.com>
2023-02-06 05:46:09 -0500

    uh huh

--- source/game/PlayState.hx
+++ source/game/PlayState.hx
@@ -10,22 +10,12 @@ class PlayState extends FlxState
 {
     var level:FlxTilemap;
  
-    override public function create():Void
+    function create():Void
     {
-        super.create();
- 
         level = new FlxTilemap();
 				level.loadMapFromCSV(AssetPaths.returnMapCSV('quiet-graveyard'), AssetPaths.returnMapTileset('quiet-graveyard'), 16, 16);
         add(level);
-    }
  
-    override public function update():Void
-    {
-        super.update();
-    }
- 
-    override public function destroy():Void
-    {
-        super.destroy();
+        super.create();
     }
 }
