abfab7975e60288c84ef8de6298ba2915632abfd
Nicholas Demetro <itssng7@gmail.com>
2023-02-06 02:51:56 -0500

    fix

--- source/backend/AssetPaths.hx
+++ source/backend/AssetPaths.hx
@@ -4,10 +4,10 @@ import openfl.utils.Assets;
 
 class AssetPaths {
  inline public static function returnMapOgmo(key:String) {
-		return Assets.getText('assets/maps/$key/map.ogmo');
+		return 'assets/maps/$key/map.ogmo';
 	}
  inline public static function returnMapJson(key:String) {
-		return Assets.getText('assets/maps/$key/map.json');
+		return 'assets/maps/$key/map.json';
 	}
  inline public static function returnMapTileset(key:String) {
 		return 'assets/maps/$key/tileset.png';
