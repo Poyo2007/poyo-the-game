2c03e3bec6fb17c3dc5280075597c7930c7c79a1
Nicholas Demetro <itssng7@gmail.com>
2023-02-06 05:31:18 -0500

    done

--- source/backend/AssetPaths.hx
+++ source/backend/AssetPaths.hx
@@ -3,11 +3,8 @@ package backend;
 import openfl.utils.Assets;
 
 class AssetPaths {
- inline public static function returnMapOgmo(key:String) {
-		return 'assets/maps/$key/map.ogmo';
-	}
- inline public static function returnMapJson(key:String) {
-		return 'assets/maps/$key/map.json';
+ inline public static function returnMapCSV(key:String) {
+		return Assets.getText('assets/maps/$key/map.csv');
 	}
  inline public static function returnMapTileset(key:String) {
 		return 'assets/maps/$key/tileset.png';
--- source/game/PlayState.hx
+++ source/game/PlayState.hx
@@ -2,62 +2,20 @@ package game;
  
 import flixel.FlxState;
 import flixel.FlxG;
-import haxe.io.Path;
 import flixel.tile.FlxTilemap;
-import flixel.addons.editors.tiled.TiledMap;
+import openfl.Assets;
  
 class PlayState extends FlxState
 {
+    var level:FlxTilemap;
+ 
     override public function create():Void
     {
         super.create();
  
-        // Load the TMX data
-        var tiledLevel:TiledMap = new TiledMap("assets/maps/quiet-graveyard/map.tmx");
- 
-        // Get map variables
-        var tileSize = tiledLevel.tileWidth;
-        var mapW = tiledLevel.width;
-        var mapH = tiledLevel.height;
- 
-        // Loop through each tile layer and render tile map
-        for (layer in tiledLevel.layers)
-        {
-            var layerData:Array<Int> = layer.tileArray;
- 						var tilesheetPath:String = "assets/maps/quiet-graveyard/tileset.png";
-            var level:FlxTilemap = new FlxTilemap();
- 
-            level.widthInTiles = mapW;
-            level.heightInTiles = mapH;
- 
-            var tileGID:Int = getStartGid(tiledLevel, tilesheetName);
- 
-            level.loadMap(layer.tileArray, tilesheetPath, tileSize, tileSize, FlxTilemap.OFF, tileGID);
-            add(level);
-        }
-    }
- 
-    function getStartGid (tiledLevel:TiledMap, tilesheetName:String):Int
-    {
-        // This function gets the starting GID of a tilesheet
- 
-        // Note: "0" is empty tile, so default to a non-empty "1" value.
-        var tileGID:Int = 1;
- 
-        for (tileset in tiledLevel.tilesets)
-        {
-            // We need to search the tileset's firstGID -- to do that,
-            // we compare the tilesheet paths. If it matches, we
-            // extract the firstGID value.
-            var tilesheetPath:Path = new Path(tileset.imageSource);
-            var thisTilesheetName = tilesheetPath.file + "." + tilesheetPath.ext;
-            if (thisTilesheetName == tilesheetName)
-            {
-                tileGID = tileset.firstGID;
-            }
-        }
- 
-        return tileGID;
+        level = new FlxTilemap();
+				level.loadMap(AssetPaths.returnMapCSV('quiet-graveyard'), AssetPaths.returnMapTileset('quiet-graveyard'), 16, 16);
+        add(level);
     }
  
     override public function update():Void
