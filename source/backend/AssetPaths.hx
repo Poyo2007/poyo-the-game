514f05ed3f9285b3236c4f6726a519084fd1116c
Nicholas Demetro <itssng7@gmail.com>
2023-02-05 23:37:15 -0500

    ya

--- source/backend/AssetPaths.hx
+++ source/backend/AssetPaths.hx
@@ -4,9 +4,9 @@ import openfl.utils.Assets;
 
 class AssetPaths {
  inline public static function returnMapCSV(key:String) {
-		return 'assets/maps/$key/map.csv';
+		return Assets.getText('assets/maps/$key/map.csv');
 	}
  inline public static function returnMapTileset(key:String) {
-		return Assets.getText('assets/maps/$key/tileset.png');
+		return 'assets/maps/$key/tileset.png'
 	}
 }
