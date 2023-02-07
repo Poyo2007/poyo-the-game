c13b8c9d6e022a58918236a55e5fba31bfdc4057
Nicholas Demetro <itssng7@gmail.com>
2023-02-07 00:01:11 -0500

    camera stuff

--- source/game/PlayState.hx
+++ source/game/PlayState.hx
@@ -1,6 +1,7 @@
 package game;
- 
+
 import flixel.FlxState;
+import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.tile.FlxTilemap;
 import openfl.Assets;
@@ -16,17 +17,39 @@ class PlayState extends FlxState
 	var flxPad:FlxVirtualPad;
 	#end
 
+	var defaultCam:FlxCamera;
+	#if mobile
+	var controlsCam:FlxCamera;
+	#end
+
 	override function create():Void
 	{
+		defaultCam = new FlxCamera();
+		#if mobile
+		controlsCam = new FlxCamera();
+		controlsCam.bgColor.alpha = 0;
+		#end
+
+		FlxG.cameras.reset(defaultCam);
+		#if mobile
+		FlxG.cameras.add(controlsCam, false);
+		#end
+
+		FlxG.cameras.setDefaultDrawTarget(defaultCam, true);
+
 		level = new FlxTilemap();
 		level.loadMapFromCSV(AssetPaths.returnMapCSV('quiet-graveyard'), AssetPaths.returnMapTileset('quiet-graveyard'), 16, 16);
 		add(level);
+
+		persistentUpdate = true;
+		persistentDraw = true;
 
 		player = new Player(0, 0);
 		add(player);
 
 		#if mobile
 		flxPad = new FlxVirtualPad(LEFT_FULL, A_B_X_Y);
+		flxPad.cameras = [controlsCam];
 		add(flxPad);
 		#end
 
