5573ff3c0a62630b6eff60ceff9126813bf20d1c
Nicholas Demetro <itssng7@gmail.com>
2023-02-07 21:35:42 -0500

    transition time

--- source/backend/AssetPaths.hx
+++ source/backend/AssetPaths.hx
@@ -1,6 +1,7 @@
 package backend;
 
 import openfl.utils.Assets;
+import backend.MetaData
 
 class AssetPaths {
  inline public static function returnMapCSV(key:String) {
@@ -8,5 +9,9 @@ class AssetPaths {
 	}
  inline public static function returnMapTileset(key:String) {
 		return 'assets/maps/$key/tileset.png';
+	}
+	inline public static function returnCharacter(key:String) {
+		var char:String = MetaData._character;
+		return 'assets/characters/$char/$key.png';
 	}
 }
---
+++ source/game/TransitionState.hx
@@ -0,0 +1,19 @@
+package game;
+
+import flixel.FlxSprite;
+import flixel.addons.display.FlxBackdrop;
+import flixel.FlxG;
+import backend.MetaData;
+import backend.AssetPaths;
+
+class TransitionState extends FlxState
+{
+	var character:FlxSprite;
+
+	override function create()
+	{
+		var player = new FlxSprite();
+		player.loadGraphic(AssetPaths.returnCharacter('transition'));
+		add(player);
+	}
+}
